9ab04f2efea8cf85f161074b78ea235cc588acdc
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-14 00:22:17 -0600

    got the tables sorted. almost have the crux figured out. too sleepy

--- index.js
+++ index.js
@@ -27,23 +27,7 @@ const departmentQuestion = [
     message: "Input your department name:",
   },
 ];
-const roleQuestion = [
-  {
-    type: "input",
-    name: "name",
-    message: "Input your role name:",
-  },
-  {
-    type: "input",
-    name: "salary",
-    message: "Input the roles salary:",
-  },
-  {
-    type: "input",
-    name: "employee",
-    message: "Choose your roles Department:",
-  },
-];
+
 
 const db = mysql2.createConnection({
   host: "127.0.0.1",
@@ -70,18 +54,45 @@ function departmentFunction() {
     mainMenu();
   });
 }
-function employeeAdd() {
-  prompt(departmentQuestion).then((data) => {
-    console.log(data);
-    const sql = `INSERT INTO department (name) VALUES (?)`;
-    const params = [data.department];
-    db.query(sql, params, (err, result) => {
-      if (err) throw err;
-    });
-    mainMenu();
-  });
-}
+
 function roleAdd() {
+  db.query("SELECT department.name AS department FROM department"), ( err, results) => {
+    if (err) throw err;
+
+    const departmentChoices = results.map(row => row.department);
+
+    const roleQuestion = [
+      {
+        type: "input",
+        name: "name",
+        message: "Input your role name:",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Input the roles salary:",
+      },
+      {
+        type: "list", 
+        name: "department",
+        message: "Choose your roles Department:",
+        choices: departmentChoices 
+      }
+    ];
+
+    prompt(roleQuestion).then((data) => {
+      console.log(data);
+      const sql = `INSERT INTO department (name) VALUES (?)`;
+      const params = [data.name];
+      db.query(sql, params, (err, result) => {
+        if (err) throw err;
+      });
+      mainMenu();
+    });
+  }
+}
+
+function employeeAdd() {
   prompt(departmentQuestion).then((data) => {
     console.log(data);
     const sql = `INSERT INTO department (name) VALUES (?)`;
@@ -99,19 +110,20 @@ function mainMenu() {
       const { options } = selection;
       switch (options) {
         case "View all departments":
-          db.query("SELECT * FROM department", function (err, results) {
+          db.query("SELECT department.name AS Department, department.id as ID FROM department", function (err, results) {
             console.table(results);
             mainMenu();
           });
           break;
         case "View all roles":
-          db.query("SELECT * FROM role LEFT JOIN dep", function (err, results) {
+          db.query("SELECT role.title AS `Job Title`, department.name AS Department, role.salary AS salary FROM role INNER JOIN department ON role.department_id = department.id", function (err, results) {
             console.table(results);
             mainMenu();
           });
           break;
         case "View all employees":
-          db.query("SELECT * FROM employee", function (err, results) {
+          db.query("SELECT employee.id AS 'Employee ID', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS 'Salary', CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager Name' FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id"
+          , function (err, results) {
             console.table(results);
             mainMenu();
           });
