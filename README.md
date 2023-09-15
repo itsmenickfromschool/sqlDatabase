03e73400beb70ac175927224a020ade0557f293b
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-14 18:54:26 -0600

    got the employee and role add functions going, need a bit more work.

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ GIVEN a command-line application that accepts user input
 
 
 
-WHEN I choose to add a role
-THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
 WHEN I choose to add an employee
 THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
--- index.js
+++ index.js
@@ -56,10 +56,15 @@ function departmentFunction() {
 }
 
 function roleAdd() {
-  db.query("SELECT department.name AS department FROM department"), ( err, results) => {
+  db.query("SELECT department.id, department.name AS department FROM department", ( err, results) => {
     if (err) throw err;
 
-    const departmentChoices = results.map(row => row.department);
+    const departmentChoices = results.map(row => {
+      return {
+        name: row.department,
+        value: row.id
+      };
+    });
 
     const roleQuestion = [
       {
@@ -74,34 +79,75 @@ function roleAdd() {
       },
       {
         type: "list", 
-        name: "department",
+        name: "department_id",
         message: "Choose your roles Department:",
         choices: departmentChoices 
       }
     ];
 
     prompt(roleQuestion).then((data) => {
-      console.log(data);
-      const sql = `INSERT INTO department (name) VALUES (?)`;
-      const params = [data.name];
+      const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`
+      const params = [data.name, data.salary, data.department_id]
       db.query(sql, params, (err, result) => {
         if (err) throw err;
       });
       mainMenu();
     });
   }
-}
+)}
 
 function employeeAdd() {
-  prompt(departmentQuestion).then((data) => {
-    console.log(data);
-    const sql = `INSERT INTO department (name) VALUES (?)`;
-    const params = [data.department];
-    db.query(sql, params, (err, result) => {
-      if (err) throw err;
+  db.query("SELECT employee.manager_id, role.title, role.id, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id", ( err, results) => {
+
+    const roleChoices = results.map(row => {
+      return {
+        name: row.title,
+        value: row.id
+      };
     });
-    mainMenu();
-  });
+
+    const managerChoices = results.map(row => {
+      return {
+        name: row.manager,
+        value: row.manager_id
+      };
+    });
+
+    const employeeQuestions = [
+      {
+        type: "input",
+        name: "first_name",
+        message: "Input your employee's fisrt name:",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Input your employee's last name",
+      },
+      {
+        type: "list",
+        name: "role_id",
+        message: "Choose your employee's role:",
+        choices: roleChoices
+      },
+      {
+        type: "list", 
+        name: "manager_id",
+        message: "Choose your employee's Manager:",
+        choices: managerChoices
+      }
+    ];
+
+
+    prompt(employeeQuestions).then((data) => {
+      const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+      const params = [data.first_name, data.last_name, data.role_id, data.manager_id];
+      db.query(sql, params, (err, result) => {
+        if (err) throw err;
+      });
+      mainMenu();
+    });
+  })
 }
 
 function mainMenu() {
@@ -130,9 +176,12 @@ function mainMenu() {
           break;
         case "Add a department":
           departmentFunction();
-          // mainMenu();
           break;
         case "Add a role":
+          roleAdd();
+          break;
+        case "Add an employee":
+          employeeAdd();
           break;
         case "Update an employee role":
           break;
@@ -140,7 +189,6 @@ function mainMenu() {
           process.exit();
       }
     })
-    .then();
 }
 
 mainMenu();
