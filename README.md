8921c08068e9cbfc6bdc60a8998e6530eea5a680
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-16 13:04:28 -0600

    added a readme and cleaned code

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-GIVEN a command-line application that accepts user input
+# Ski Resort Database Content Management System
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## [Video of app]()
+  ## Description
+  This project was about making a content management system for a business. I used Inquirer as well as mysql2 for the project. The App in entirely run in the backend. There is no forward-facing User-Interface. The App can be used to create and view roles, departments, and employees within a SQL database. Additionally, you can update an employee's role. Throughout building this app I learned how to interface with a SQL database through Javascript. I also learned a lot about SQL in general, speicifically Querying as well as seeding the data in the right order so the relationships between the foreign and primary keys was succesful. Moving forward, I would refactor my code to use async/await instead of relying on callback functions, additionally I would add more features such as the ability to delete employees, roles, and departments. 
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Credits](#credits)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  In order to Install this on your machine, clone the repository and run it in your IDE of choice. It is important to make sure you have MYSQL installed on your computer. To do that you can simply google instructions from Oracle, They are the creators of MYSQL. 
+  ## Usage 
+  To use this app run the terminal command mysql -u root -p , then enter your mysql password. Next, source the schema file with the command SOURCE schema.sql; next source the seeds file with the command SOURCE seeds.sql; Now you can type quit and press enter to exit the MYSQL CLI. The next command is node index.js to run the CMS system, which is setup to run the database for a mock ski resort. Simply follow the prompts to use the application. If you want to Quit out of the application you can do so with an option in the menu, or you can do ctrl-c to exit the process manually.
+  ## Credits
+  This project was completed by me, Nicholas Burr. I had a little bit of help at the start from a EDX tutor, Dru Sanchez, but all of the code is my own. 
+  ## License
+  This application is covered under the MIT license, for more information please click the badge at the top of this README, which has a link to everything about the license.
+  ## Tests
+  
+  ## Questions
+  My [Github Profile](https://github.com/itsmenickfromschool)
+  Additionally you can email me at <nicholasstclairburr@gmail.com> with any questions!
 
 
 
@@ -7,10 +34,3 @@ GIVEN a command-line application that accepts user input
 
 
 
-
-
-WHEN I choose to add an employee
-THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-WHEN I choose to update an employee role
-THEN I am prompted to select an employee to update and their new role and this information is updated in the database
--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const departmentQuestion = [
   },
 ];
 
-
 const db = mysql2.createConnection({
   host: "127.0.0.1",
   user: "root",
@@ -40,12 +39,8 @@ db.connect((err) => {
   console.log("connected!");
 });
 
-// db.query(queryString, function (err, results) {
-//   // console.log(results);
-// });
 function departmentFunction() {
   prompt(departmentQuestion).then((data) => {
-    console.log(data);
     const sql = `INSERT INTO department (name) VALUES (?)`;
     const params = [data.department];
     db.query(sql, params, (err, result) => {
@@ -56,139 +51,213 @@ function departmentFunction() {
 }
 
 function roleAdd() {
-  db.query("SELECT department.id, department.name AS department FROM department", ( err, results) => {
-    if (err) throw err;
+  db.query(
+    "SELECT department.id, department.name AS department FROM department",
+    (err, results) => {
+      if (err) throw err;
 
-    const departmentChoices = results.map(row => {
-      return {
-        name: row.department,
-        value: row.id
-      };
-    });
-
-    const roleQuestion = [
-      {
-        type: "input",
-        name: "name",
-        message: "Input your role name:",
-      },
-      {
-        type: "input",
-        name: "salary",
-        message: "Input the roles salary:",
-      },
-      {
-        type: "list", 
-        name: "department_id",
-        message: "Choose your roles Department:",
-        choices: departmentChoices 
-      }
-    ];
-
-    prompt(roleQuestion).then((data) => {
-      const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`
-      const params = [data.name, data.salary, data.department_id]
-      db.query(sql, params, (err, result) => {
-        if (err) throw err;
+      const departmentChoices = results.map((row) => {
+        return {
+          name: row.department,
+          value: row.id,
+        };
       });
-      mainMenu();
-    });
-  }
-)}
 
-function employeeAdd() {
-  db.query("SELECT employee.manager_id, role.title, role.id, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id", ( err, results) => {
+      const roleQuestion = [
+        {
+          type: "input",
+          name: "name",
+          message: "Input your role name:",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Input the roles salary:",
+        },
+        {
+          type: "list",
+          name: "department_id",
+          message: "Choose your roles Department:",
+          choices: departmentChoices,
+        },
+      ];
 
-    const roleChoices = results.map(row => {
-      return {
-        name: row.title,
-        value: row.id
-      };
-    });
-
-    const managerChoices = results.map(row => {
-      return {
-        name: row.manager,
-        value: row.manager_id
-      };
-    });
-
-    const employeeQuestions = [
-      {
-        type: "input",
-        name: "first_name",
-        message: "Input your employee's fisrt name:",
-      },
-      {
-        type: "input",
-        name: "last_name",
-        message: "Input your employee's last name",
-      },
-      {
-        type: "list",
-        name: "role_id",
-        message: "Choose your employee's role:",
-        choices: roleChoices
-      },
-      {
-        type: "list", 
-        name: "manager_id",
-        message: "Choose your employee's Manager:",
-        choices: managerChoices
-      }
-    ];
-
-
-    prompt(employeeQuestions).then((data) => {
-      const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-      const params = [data.first_name, data.last_name, data.role_id, data.manager_id];
-      db.query(sql, params, (err, result) => {
-        if (err) throw err;
+      prompt(roleQuestion).then((data) => {
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        const params = [data.name, data.salary, data.department_id];
+        db.query(sql, params, (err, result) => {
+          if (err) throw err;
+        });
+        mainMenu();
       });
-      mainMenu();
-    });
-  })
+    }
+  );
 }
 
-function mainMenu() {
-  prompt(menu)
-    .then((selection) => {
-      const { options } = selection;
-      switch (options) {
-        case "View all departments":
-          db.query("SELECT department.name AS Department, department.id as ID FROM department", function (err, results) {
-            console.table(results);
-            mainMenu();
+function employeeAdd() {
+  // First, fetch roles
+  db.query("SELECT role.title, role.id FROM role", (err, rolesResults) => {
+    if (err) throw err;
+
+    const roleChoices = rolesResults.map((row) => {
+      return {
+        name: row.title,
+        value: row.id,
+      };
+    });
+
+    db.query(
+      "SELECT DISTINCT manager.id AS manager_id, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee AS manager WHERE manager.id IN (SELECT DISTINCT manager_id FROM employee WHERE manager_id IS NOT NULL)",
+      (err, managerResults) => {
+        if (err) throw err;
+
+        const managerChoices = managerResults.map((row) => {
+          return {
+            name: row.manager_name,
+            value: row.manager_id,
+          };
+        });
+
+        const employeeQuestions = [
+          {
+            type: "input",
+            name: "first_name",
+            message: "Input your employee's first name:",
+          },
+          {
+            type: "input",
+            name: "last_name",
+            message: "Input your employee's last name",
+          },
+          {
+            type: "list",
+            name: "role_id",
+            message: "Choose your employee's role:",
+            choices: roleChoices,
+          },
+          {
+            type: "list",
+            name: "manager_id",
+            message: "Choose your employee's Manager:",
+            choices: managerChoices,
+          },
+        ];
+
+        prompt(employeeQuestions).then((data) => {
+          const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+          const params = [
+            data.first_name,
+            data.last_name,
+            data.role_id,
+            data.manager_id,
+          ];
+          db.query(sql, params, (err, result) => {
+            if (err) throw err;
           });
-          break;
-        case "View all roles":
-          db.query("SELECT role.title AS `Job Title`, department.name AS Department, role.salary AS salary FROM role INNER JOIN department ON role.department_id = department.id", function (err, results) {
-            console.table(results);
-            mainMenu();
-          });
-          break;
-        case "View all employees":
-          db.query("SELECT employee.id AS 'Employee ID', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS 'Salary', CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager Name' FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id"
-          , function (err, results) {
-            console.table(results);
-            mainMenu();
-          });
-          break;
-        case "Add a department":
-          departmentFunction();
-          break;
-        case "Add a role":
-          roleAdd();
-          break;
-        case "Add an employee":
-          employeeAdd();
-          break;
-        case "Update an employee role":
-          break;
-        case "Quit":
-          process.exit();
+          mainMenu();
+        });
       }
-    })
+    );
+  });
+}
+
+function employeeUpdate() {
+  db.query("SELECT role.title, role.id FROM role", (err, results) => {
+    if (err) throw err;
+
+    const roleChoices = results.map((row) => {
+      return {
+        name: row.title,
+        value: row.id,
+      };
+    });
+
+    db.query(
+      "SELECT DISTINCT CONCAT(employee.first_name, ' ', employee.last_name) AS employee_name, employee.id FROM employee",
+      (err, results) => {
+        if (err) throw err;
+
+        const employeeChoices = results.map((row) => {
+          return {
+            name: row.employee_name,
+            value: row.id,
+          };
+        });
+
+        const employeeQuestions = [
+          {
+            type: "list",
+            name: "employee_ID",
+            message: "Choose an employee:",
+            choices: employeeChoices,
+          },
+          {
+            type: "list",
+            name: "role",
+            message: "Choose the employees new role:",
+            choices: roleChoices,
+          },
+        ];
+        prompt(employeeQuestions).then((data) => {
+          const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+          const params = [data.role, data.employee_ID];
+          db.query(sql, params, (err, result) => {
+            if (err) throw err;
+          });
+        mainMenu();
+        });
+      }
+    );
+  });
+};
+
+function mainMenu() {
+  prompt(menu).then((selection) => {
+    const { options } = selection;
+    switch (options) {
+      case "View all departments":
+        db.query(
+          "SELECT department.name AS Department, department.id as ID FROM department",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
+        break;
+      case "View all roles":
+        db.query(
+          "SELECT role.title AS `Job Title`, department.name AS Department, role.salary AS salary FROM role INNER JOIN department ON role.department_id = department.id",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
+        break;
+      case "View all employees":
+        db.query(
+          "SELECT employee.id AS 'Employee ID', employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Job Title', department.name AS 'Department', role.salary AS 'Salary', CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager Name' FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id",
+          function (err, results) {
+            console.table(results);
+            mainMenu();
+          }
+        );
+        break;
+      case "Add a department":
+        departmentFunction();
+        break;
+      case "Add a role":
+        roleAdd();
+        break;
+      case "Add an employee":
+        employeeAdd();
+        break;
+      case "Update an employee role":
+        employeeUpdate();
+        break;
+      case "Quit":
+        process.exit();
+    }
+  });
 }
 
 mainMenu();
